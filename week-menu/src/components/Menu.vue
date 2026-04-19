c258f10b260fd6a82c4d23aa8196df1c80096145
lse-odoo <lse@odoo.com>
2026-04-19 14:39:39 +0200

    basic install and basic view

---
+++ week-menu/src/components/Menu.vue
@@ -0,0 +1,14 @@
+<script setup>
+import menuData from '../menu-data.json'
+</script>
+
+<template>
+    <div v-for="menu in menuData.menus" :key="menu.name">
+        <h2>{{ menu.name }}</h2>
+        <ul>
+            <li v-for="ingredient in menu.ingredients" :key="ingredient.item">
+                {{ ingredient.item }} - {{ ingredient.quantity }} {{ ingredient.unit }}
+            </li>
+        </ul>
+    </div>
+</template>
